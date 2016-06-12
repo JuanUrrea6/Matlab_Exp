27078580f5453c96c29983fbe8a49c164cc50c3f
Juan Andrés Urrea <ja.urrea10@uniandes.edu.co>
2016-06-12 18:59:13 -0500

    Primer punto :leaves:

---
+++ C1/P1.m
@@ -0,0 +1,14 @@
+function P1
+disp(43^2);
+disp(-3^4);
+disp(sin(1));
+disp(4^(3^2));
+disp((-3)^4);
+disp(sind(1));
+disp((4^3)^2);
+disp((-3)^(1/4));
+disp(sin(pi/3));
+disp(32^(1/4));
+disp(-2^(-4/3));
+disp(asin(1)/pi);
+end
