f26dd415667b0d0fa94f4665a069dc963bd36044
Juan Andrés Urrea <ja.urrea10@uniandes.edu.co>
2016-06-12 20:36:26 -0500

    Puntos 2 y 5.

---
+++ C1/P2.m
@@ -0,0 +1,4 @@
+function [newF,newC] = P2(c,f)
+newF = 1.8*c + 32;
+newC = (f-32)/1.8;
+end
---
+++ C1/P5.m
@@ -0,0 +1,9 @@
+function P5
+A = pi^((1+(5)^(1/2))/2);
+B = ((1+5^(1/2))/2)^pi;
+M = 'pi^phi es mayor';
+if(B > A)
+    M = 'phi^pi es mayor';
+end
+disp(M);
+end
